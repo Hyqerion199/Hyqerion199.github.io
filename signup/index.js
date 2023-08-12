56d4adfb210268498e5a4a839670fdf7a38b2706
Hyqerion199 <matjel08@gmail.com>
2023-08-12 14:40:19 -0400

    Update index.js

--- signup/index.js
+++ signup/index.js
@@ -4,5 +4,5 @@ const supabaseUrl = 'https://nehibeqlnydjxvhpcfml.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5laGliZXFsbnlkanh2aHBjZm1sIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA4MjI5MTEsImV4cCI6MjAwNjM5ODkxMX0.vGZqm7G6cvJXGMu9hvDXqWf8IJ8Sh3bSuufb4WhXlxU'
 let _supabase = supabase.createClient(supabaseUrl, supabaseKey)
 
-console.log(supabase)
+console.log(_supabase)
 
