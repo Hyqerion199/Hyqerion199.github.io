1dd137949eff61f7ae68b245b611fdae92b79902
Hyqerion199 <matjel08@gmail.com>
2023-08-12 14:22:15 -0400

    Update index.js

--- signup/index.js
+++ signup/index.js
@@ -1,4 +1,4 @@
-import createClient from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2'
+import createClient from '../supabase.js'
 
 const supabaseUrl = 'https://nehibeqlnydjxvhpcfml.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5laGliZXFsbnlkanh2aHBjZm1sIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA4MjI5MTEsImV4cCI6MjAwNjM5ODkxMX0.vGZqm7G6cvJXGMu9hvDXqWf8IJ8Sh3bSuufb4WhXlxU'
