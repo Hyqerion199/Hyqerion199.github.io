43fcb6b80963705a96bb3a271f0af20b906d1259
Hyqerion199 <matjel08@gmail.com>
2023-08-12 14:09:53 -0400

    idk

--- signup/index.js
+++ signup/index.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import createClient from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://nehibeqlnydjxvhpcfml.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5laGliZXFsbnlkanh2aHBjZm1sIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA4MjI5MTEsImV4cCI6MjAwNjM5ODkxMX0.vGZqm7G6cvJXGMu9hvDXqWf8IJ8Sh3bSuufb4WhXlxU'
