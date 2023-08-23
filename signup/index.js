dc1fcf8fed16a3dc4887a99063c939e23e40242b
Hyqerion199 <78224761+Hyqerion199@users.noreply.github.com>
2023-08-23 13:13:59 -0400

    some more work

--- signup/index.js
+++ signup/index.js
@@ -1,10 +1,18 @@
 let emailfield = document.getElementsByName("emailfield");
+let userfield = document.getElementsByName("userfield");
+let pwdfield = document.getElementsByName("pwdfield");
 
 function test(){
     if (emailfield[0].value == ""){
         alert("Please enter an email");
     }
+    if (userfield[0].value == ""){
+        alert("Please enter a Username");
+    }
+    if (pwdfield[0].value == ""){
+        alert("Please enter a password");
+    }
     else {
-    alert(emailfield[0].value)
+    alert(`Your email is '${emailfield[0].value}' your username is '${userfield[0].value}' and your password is '${pwdfield[0].value}'`);
     }
 }
