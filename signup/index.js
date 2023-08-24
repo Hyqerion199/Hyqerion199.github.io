641d969d6d05715fef052332612f575734e9f9db
Hyqerion199 <matjel08@gmail.com>
2023-08-23 21:18:05 -0400

    worked on signup page

--- signup/index.js
+++ signup/index.js
@@ -2,7 +2,7 @@ let emailfield = document.getElementsByName("emailfield");
 let userfield = document.getElementsByName("userfield");
 let pwdfield = document.getElementsByName("pwdfield");
 
-function test(){
+function signup(){
     if (emailfield[0].value == ""){
         alert("Please enter an email");
     }
@@ -15,4 +15,38 @@ function test(){
     else {
     alert(`Your email is '${emailfield[0].value}' your username is '${userfield[0].value}' and your password is '${pwdfield[0].value}'`);
     }
+}
+
+function login(){
+    if (emailfield[0].value == ""){
+        alert("Please enter an email");
+    }
+    if (pwdfield[0].value == ""){
+        alert("Please enter a password");
+    }
+    else {
+    alert(`Your email is '${emailfield[0].value}' and your password is '${pwdfield[0].value}'`);
+    }
+}
+
+function switchhidden(page){
+    if (page == "signup"){
+        document.getElementById("loginform").classList.toggle('hide');
+        document.getElementById("signupform").classList.toggle('hide');
+        localStorage.setItem('typeofpage', 'login');
+    }
+    if (page == "login"){
+        document.getElementById("loginform").classList.toggle('hide');
+        document.getElementById("signupform").classList.toggle('hide');
+        localStorage.setItem('typeofpage', 'signup');
+    }
+}
+
+//iff the data doesnt exist 
+if (localStorage.getItem('typeofpage') == null){
+    localStorage.setItem('typeofpage', 'signup');
+}
+if (localStorage.getItem('typeofpage') == 'login'){
+    document.getElementById("loginform").classList.remove('hide');
+    document.getElementById("signupform").classList.add('hide');
 }
