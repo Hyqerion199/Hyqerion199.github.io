8e7af690198130c29f9236749e0a143495de7732
Hyqerion199 <matjel08@gmail.com>
2023-08-24 19:41:50 -0400

    idk

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ async function logout(){
 
 async function newload(){
     
- if (localStorage.getItem('sb-nehibeqlnydjxvhpcfml-auth-token')) {
-    const { data, error } = await supabase.auth.getSession()
-    document.getElementById("signupbutton").classList.add('hide');
-    document.getElementById("loginbutton").classList.add('hide');
-    document.getElementById("logoutbutton").classList.remove('hide');
-    document.getElementById("mainhead").style.left = "56px";
-    console.log(data)
- }
-}
-
-newload();
+    if (localStorage.getItem('sb-nehibeqlnydjxvhpcfml-auth-token')) {
+       const { data, error } = await supabase.auth.getSession()
+       document.getElementById("signupbutton").classList.add('hide');
+       document.getElementById("loginbutton").classList.add('hide');
+       document.getElementById("logoutbutton").classList.remove('hide');
+       document.getElementById("mainhead").style.left = "56px";
+       console.log(data)
+    }
+   }
+   
+   newload();
