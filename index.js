4ac0a4bafb20f4e692d513c3e57266b7f4e67cb7
Hyqerion199 <matjel08@gmail.com>
2023-08-25 17:12:53 -0400

    formatted code

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-async function logout(){
+async function logout() {
     await _supabase.auth.signOut();
     window.location.assign("/");
-    
+
 }
 
+async function newload() {
 
-async function newload(){
-    
     if (localStorage.getItem('sb-nehibeqlnydjxvhpcfml-auth-token')) {
-       const { data, error } = await _supabase.auth.getSession()
-       document.getElementById("signupbutton").classList.add('hide');
-       document.getElementById("loginbutton").classList.add('hide');
-       document.getElementById("logoutbutton").classList.remove('hide');
-       document.getElementById("mainhead").style.left = "56px";
-       console.log(data)
+        const { data, error } = await _supabase.auth.getSession()
+        document.getElementById("signupbutton").classList.add('hide');
+        document.getElementById("loginbutton").classList.add('hide');
+        document.getElementById("logoutbutton").classList.remove('hide');
+        document.getElementById("mainhead").style.left = "56px";
+        console.log(data)
     }
-   }
-   
-   newload();
+}
+
+newload();
--- signup/index.js
+++ signup/index.js
@@ -6,65 +6,65 @@ let alertid = document.getElementById("alert");
 
 let alertmessage = ["Please enter a Username.", "Please enter an Name/Nickname.", "Please enter an email.", "Please enter a password.", "Please enter a valid email.", "Please enter a password with at least 8 characters."];
 
-async function signup(){
+async function signup() {
     emailstring = emailfield[0].value;
     pwdstring = pwdfield[0].value;
-    if (userfield[0].value == ""){
+    if (userfield[0].value == "") {
         alertid.innerHTML = alertmessage[0];
         alertid.classList.remove('hide');
     }
-    else if (namefield[0].value == ""){
+    else if (namefield[0].value == "") {
         alertid.innerHTML = alertmessage[1];
         alertid.classList.remove('hide');
     }
-    else if (emailfield[0].value == ""){
+    else if (emailfield[0].value == "") {
         alertid.innerHTML = alertmessage[2];
         alertid.classList.remove('hide');
     }
-    else if (pwdfield[0].value == ""){
+    else if (pwdfield[0].value == "") {
         alertid.innerHTML = alertmessage[3];
         alertid.classList.remove('hide');
     }
-    else if (emailstring.indexOf("@") == -1 || emailstring.indexOf(".") == -1){
+    else if (emailstring.indexOf("@") == -1 || emailstring.indexOf(".") == -1) {
         alertid.innerHTML = alertmessage[4];
         alertid.classList.remove('hide');
     }
-    else if (pwdstring.length < 8){
+    else if (pwdstring.length < 8) {
         alertid.innerHTML = alertmessage[5];
         alertid.classList.remove('hide');
     }
     else {
-    alertid.classList.add('hide');
-    const { data, error } = await _supabase.auth.signUp({
-        email: emailfield[0].value,
-        password: pwdfield[0].value,
-        options: {
-            data: {
-                username: userfield[0].value,
-                name: namefield[0].value
+        alertid.classList.add('hide');
+        const { data, error } = await _supabase.auth.signUp({
+            email: emailfield[0].value,
+            password: pwdfield[0].value,
+            options: {
+                data: {
+                    username: userfield[0].value,
+                    name: namefield[0].value
+                }
             }
-        }
-    })
-    //window.location.assign("/");
+        })
+        //window.location.assign("/");
     }
 }
 
-async function login(){
+async function login() {
     emailstring = emailfield[1].value;
     pwdstring = pwdfield[1].value;
-    if (emailfield[1].value == ""){
+    if (emailfield[1].value == "") {
         alertid.innerHTML = alertmessage[2];
         alertid.classList.remove('hide');
     }
-    else if (pwdfield[1].value == ""){
+    else if (pwdfield[1].value == "") {
         alertid.innerHTML = alertmessage[3];
         alertid.classList.remove('hide');
     }
-    else if (emailstring.indexOf("@") == -1 || emailstring.indexOf(".") == -1){
+    else if (emailstring.indexOf("@") == -1 || emailstring.indexOf(".") == -1) {
         alertid.innerHTML = alertmessage[4];
         alertid.classList.remove('hide');
     }
-    else if (pwdstring.length < 8){
+    else if (pwdstring.length < 8) {
         alertid.innerHTML = alertmessage[5];
         alertid.classList.remove('hide');
     }
@@ -73,19 +73,19 @@ async function login(){
         const { data, error } = await _supabase.auth.signInWithPassword({
             email: emailfield[1].value,
             password: pwdfield[1].value,
-          })    
-          window.location.assign("/");
-        }
+        })
+        window.location.assign("/");
+    }
 }
 
-function switchhidden(page){
-    if (page == "signup"){ //switches to Login page
+function switchhidden(page) {
+    if (page == "signup") { //switches to Login page
         document.getElementById("loginform").classList.toggle('hide');
         document.getElementById("loginheader").classList.toggle('hide');
         document.getElementById("signupform").classList.toggle('hide');
         document.getElementById("signupheader").classList.toggle('hide');
     }
-    if (page == "login"){ //switches to Signup page
+    if (page == "login") { //switches to Signup page
         document.getElementById("loginform").classList.toggle('hide');
         document.getElementById("loginheader").classList.toggle('hide');
         document.getElementById("signupform").classList.toggle('hide');
@@ -94,15 +94,15 @@ function switchhidden(page){
 }
 
 //iff the data doesnt exist 
-if (localStorage.getItem('typeofpage') == null){
+if (localStorage.getItem('typeofpage') == null) {
     localStorage.setItem('typeofpage', 'login');
 }
-if (localStorage.getItem('typeofpage') == 'login'){
+if (localStorage.getItem('typeofpage') == 'login') {
     document.getElementById("signupform").classList.add('hide');
     document.getElementById("signupheader").classList.add('hide');
     document.getElementById("TitleElement").innerHTML = "Login";
 }
-if (localStorage.getItem('typeofpage') == 'signup'){
+if (localStorage.getItem('typeofpage') == 'signup') {
     document.getElementById("loginform").classList.add('hide');
     document.getElementById("loginheader").classList.add('hide');
     document.getElementById("TitleElement").innerHTML = "Signup";
