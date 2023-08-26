cbce39cdc32626ae194701681a3f89376e4ed46a
Hyqerion199 <matjel08@gmail.com>
2023-08-26 14:28:46 -0400

    send sends a message
    chat doesnt laod anything

--- chat/index.js
+++ chat/index.js
@@ -1,3 +1,7 @@
+const messagebut = document.getElementById('messagearea')
+
+
+
 async function logout(){
     await _supabase.auth.signOut();
     window.location.assign("/");
@@ -16,8 +20,32 @@ async function newload(){
        document.getElementById("mainhead").style.left = "56px";
        document.getElementById("chat-container").style.height = "75%";
        document.getElementById("chat-container").style.zIndex = "1";
-       console.log(data)
     }
+
+
+    
+        
+
 }
    
+
+async function test(){
+    const { data, error } = await _supabase.from('chat').select()
+    console.log(data);
+}
+
+
+async function sendmessage(){
+    if (messagebut.value != ""){
+    const { data, error } = await _supabase.auth.getSession()
+    console.log(data['session']['user']['user_metadata']['name']);
+    const { data :data1, error: error1 } = await _supabase.from('chat').insert([{message: messagebut.value, name: data['session']['user']['user_metadata']['name']}])
+    messagebut.value = "";
+    console.log(data1);
+    }
+}
+
+
+
+test();
 newload();
